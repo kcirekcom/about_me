996947a3d76b7f9c1f796dccc6dae7e9beddb791
kcirekcom <erick.f.mock@gmail.com>
2016-09-20 12:15:53 -0700

    scaffold html, js, css files; delete old html file

---
+++ app.js
@@ -0,0 +1,15 @@
+var username = prompt('What is your name?');
+alert('Welcome, ' + username + '!');
+console.log('The user\'s name is ' + username);
+
+var motherBorn = prompt('Where was your mother born?');
+alert('Welcome, ' + username + '! ' + 'Your mother was born in ' + motherBorn + '!');
+console.log('The user\'s mother was born in ' + motherBorn);
+
+var favSport = prompt('What is your favorite sport?');
+alert('Welcome, ' + username + '! ' + 'Your mother was born in ' + motherBorn + ' and your favorite sport is ' + favSport + '!');
+console.log('The user\'s favorite sport is ' + favSport);
+
+var hobby = prompt('What do you like to do in your free time?');
+alert('Welcome, ' + username + '! ' + 'Your mother was born in ' + motherBorn + ' and your favorite sport is ' + favSport + '!' + ' I also enjoy ' + hobby + '!');
+console.log('The user enjoys ' + hobby);
