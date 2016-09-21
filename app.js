9ebd592f0a91755b4131d4987199b901d3ae32a5
kcirekcom <erick.f.mock@gmail.com>
2016-09-21 10:12:31 -0700

    syntax of js improved, semi-colons added, console log more specific

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    second variable called in if else statement */
 var firstQuestion = prompt('Hello! Welcome to Erick\'s Guessing Game! Let\'s get started with an easy question. Is my name Erick?');
 var firstAnswer = firstQuestion.toLowerCase();
-console.log('The user\'s answer is ' + firstAnswer + '.');
+console.log('The user\'s answer is ' + firstAnswer + '.' + ' The correct answer is yes.');
 if (firstAnswer === 'yes' || firstAnswer === 'y') {
   alert('Good. You better have gotten this answer correct.')
 } else {
@@ -12,38 +12,38 @@ if (firstAnswer === 'yes' || firstAnswer === 'y') {
 
 var secondQuestion = prompt('Have I ever lived in the Netherlands?');
 var secondAnswer = secondQuestion.toLowerCase();
-console.log('The user\'s answer is ' + secondAnswer + '.');
+console.log('The user\'s answer is ' + secondAnswer + '.' + ' The correct answer is no.');
 if (secondAnswer === 'no' || secondAnswer === 'n') {
-  alert('You got this one right! Great job!')
+  alert('You got this one right! Great job!');
 } else {
-  alert('Wrong! But I did live in Germany when I was younger ;)')
+  alert('Wrong! But I did live in Germany when I was younger.');
 }
 
 var thirdQuestion = prompt('Do I have 30 baseball caps?');
 var thirdAnswer = thirdQuestion.toLowerCase();
-console.log('The user\'s answer is ' + thirdAnswer + '.');
+console.log('The user\'s answer is ' + thirdAnswer + '.' + ' The correct answer is no.');
 if (thirdAnswer === 'no' || thirdAnswer === 'n') {
-  alert('Right! I only have 22 baseball caps.')
+  alert('Right! I only have 22 baseball caps.');
 } else {
-  alert('Your answer is wrong. Unfortunately, I only have 22 baseball caps.')
+  alert('Your answer is wrong. Unfortunately, I only have 22 baseball caps.');
 }
 
 var fourthQuestion = prompt('Before Code Fellows, I worked at Forever 21\'s Corporate Headquarters. Yes or No?');
 var fourthAnswer = fourthQuestion.toLowerCase();
-console.log('The user\'s answer is ' + fourthAnswer + '.');
+console.log('The user\'s answer is ' + fourthAnswer + '.' + ' The correct answer is yes.');
 if (fourthAnswer === 'yes' || fourthAnswer === 'y') {
-  alert('Correct! I worked in the HR department, specifically in internal communications.')
+  alert('Correct! I worked in the HR department, specifically in internal communications.');
 } else {
-  alert('That answer is incorrect!')
+  alert('That answer is incorrect!');
 }
 
 var fifthQuestion = prompt('Do I like dogs more than cats?');
 var fifthAnswer = fifthQuestion.toLowerCase();
-console.log('The user\'s answer is ' + fifthAnswer + '.');
+console.log('The user\'s answer is ' + fifthAnswer + '.' + ' The correct answer is yes.');
 if (fifthAnswer === 'yes' || fifthAnswer === 'y') {
-  alert('Right on! Dogs are the best!')
+  alert('Right on! Dogs are the best!');
 } else {
-  alert('Wrong. I\'m not much of a cat fan.')
+  alert('Wrong. I\'m not much of a cat fan.');
 }
 
-var endStatement = confirm('You have completed Erick\'s Guessing Game! Thanks for playing!')
+var endStatement = confirm('You have completed Erick\'s Guessing Game! Thanks for playing!');
