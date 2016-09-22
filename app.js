5f16708112ed4b25b721e9cda840c6caa373ff84
kcirekcom <erick.f.mock@gmail.com>
2016-09-21 17:06:17 -0700

    sixth answer added

--- app.js
+++ app.js
@@ -1,49 +1,68 @@
 'use strict';
-/* second variable applies .toLowerCase to the first variable
-   second variable called in if else statement */
-var firstQuestion = prompt('Hello! Welcome to Erick\'s Guessing Game! Let\'s get started with an easy question. Is my name Erick?');
-var firstAnswer = firstQuestion.toLowerCase();
-console.log('The user\'s answer is ' + firstAnswer + '.' + ' The correct answer is yes.');
+
+var userName = prompt('What is your name?');
+var firstAnswer = prompt('Hello, ' + userName + '! Welcome to Erick\'s Guessing Game! Let\'s get started with an easy question. Is my name Erick?').toLowerCase();
 if (firstAnswer === 'yes' || firstAnswer === 'y') {
   alert('Good. You better have gotten this answer correct.');
+  console.log(userName + ' answered this question correctly!');
 } else {
   alert('That answer is incorrect. Wow. How on earth did you get that wrong?');
+  console.log(userName + ' answered this question incorrectly.');
 }
 
-var secondQuestion = prompt('Have I ever lived in the Netherlands?');
-var secondAnswer = secondQuestion.toLowerCase();
-console.log('The user\'s answer is ' + secondAnswer + '.' + ' The correct answer is no.');
+var secondAnswer = prompt('Have I ever lived in the Netherlands?').toLowerCase();
 if (secondAnswer === 'no' || secondAnswer === 'n') {
   alert('You got this one right! Great job!');
+  console.log(userName + ' answered this question correctly!');
 } else {
   alert('Wrong! But I did live in Germany when I was younger.');
+  console.log(userName + ' answered this question incorrectly.');
 }
 
-var thirdQuestion = prompt('Do I have 30 baseball caps?');
-var thirdAnswer = thirdQuestion.toLowerCase();
-console.log('The user\'s answer is ' + thirdAnswer + '.' + ' The correct answer is no.');
+var thirdAnswer = prompt('Do I have 30 baseball caps?').toLowerCase();
 if (thirdAnswer === 'no' || thirdAnswer === 'n') {
   alert('Right! I only have 22 baseball caps.');
+  console.log(userName + ' answered this question correctly!');
 } else {
   alert('Your answer is wrong. Unfortunately, I only have 22 baseball caps.');
+  console.log(userName + ' answered this question incorrectly.');
 }
 
-var fourthQuestion = prompt('Before Code Fellows, I worked at Forever 21\'s Corporate Headquarters. Yes or No?');
-var fourthAnswer = fourthQuestion.toLowerCase();
-console.log('The user\'s answer is ' + fourthAnswer + '.' + ' The correct answer is yes.');
+var fourthAnswer = prompt('Before Code Fellows, I worked at Forever 21\'s Corporate Headquarters. Yes or No?').toLowerCase();
 if (fourthAnswer === 'yes' || fourthAnswer === 'y') {
   alert('Correct! I worked in the HR department, specifically in internal communications.');
+  console.log(userName + ' answered this question correctly!');
 } else {
   alert('That answer is incorrect!');
+  console.log(userName + ' answered this question incorrectly.');
 }
 
-var fifthQuestion = prompt('Do I like dogs more than cats?');
-var fifthAnswer = fifthQuestion.toLowerCase();
-console.log('The user\'s answer is ' + fifthAnswer + '.' + ' The correct answer is yes.');
+var fifthAnswer = prompt('Do I like dogs more than cats?').toLowerCase();
 if (fifthAnswer === 'yes' || fifthAnswer === 'y') {
   alert('Right on! Dogs are the best!');
+  console.log(userName + ' answered this question correctly.');
 } else {
   alert('Wrong. I\'m not much of a cat fan.');
+  console.log(userName + ' answered this question incorrectly.');
 }
 
-var endStatement = confirm('You have completed Erick\'s Guessing Game! Thanks for playing!');
+var favNumber = 5;
+var attempts = 0;
+while (attempts < 4) {
+  attempts++;
+  var sixthAnswer = parseInt(prompt('What is my favorite number? You will be given 4 chances to guess the correct number.'));
+  if (sixthAnswer === 5) {
+    alert('Great guess! You are correct!');
+    console.log(userName + ' answered this question correctly.');
+    break;
+  } else if (sixthAnswer < 5) {
+    alert('Your guess is too low!');
+  } else {
+    alert('Your guess is too high!');
+  }
+}
+
+var 
+
+
+// var endStatement = confirm('You have completed Erick\'s Guessing Game, ' + userName + '! Thanks for playing!');
